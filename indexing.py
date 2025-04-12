92d1fa0f90d42c79b8a4c3815ad9742e50fb649e
Kazi Jawad <Kazijawadulislam@gmail.com>
2025-04-12 20:25:10 +0600

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,13 @@
+name='Kazi Jawad'
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+print(name[9])
+print(name[10]) #10 will not print because there is no 10th index. Indexing starts from 0.
