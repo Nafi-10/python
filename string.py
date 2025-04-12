e6b69dac7687371e14e509f27fc733a556625af6
Kazi Jawad <Kazijawadulislam@gmail.com>
2025-04-12 20:24:29 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,20 @@
+name = 'Kazi Jawad'
+adress = "Khulna , Bangladesh"
+
+name2=input("Enter your name: ")
+print('My name is ' + name2)
+
+name3='I said that,"I will not go there"'
+print(name3)
+
+#multiple line string
+multipleLine="""Beneath a sky of fading light,  
+The stars awake to kiss the night.  
+A whisper drifts from tree to tree,  
+A song of hope, so soft, so free.
+
+The moon leans low with silver grace,  
+To light the dream on every face.  
+And in the hush, the heart can hear—  
+The quiet voice that draws it near."""
+print(multipleLine)
