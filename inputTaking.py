6bd09e1e5dc08040c8c87e95b1802a7eff38a6e3
Kazi Jawad <Kazijawadulislam@gmail.com>
2025-04-12 20:02:34 +0600

    Add files via upload

---
+++ inputTaking.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print('My name is ' + name)
+
+number = int(input("Enter a number: "))
+print('My number is ' + str(number))
+
+number1 = int(input("Enter a number: "))
+print('My number is ' + str(number1))
+
+print('Your total number is:' + str(number + number1))
